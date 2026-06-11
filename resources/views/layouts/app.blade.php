3233b959a028bb816f7585634e2ef9d242e5ad9d
Simarjot Singh <baljeetsimarsingh@gmail.com>
2026-06-11 17:41:19 +0530

    chore: vendorize bootstrap, jquery, gsap, fontawesome locally; update layout to use local assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Blog Dashboard')</title>
 
-    <!-- Bootstrap 5 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap 5 CSS (local fallback) -->
+    <link href="{{ asset('vendor/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
     
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- Font Awesome (local fallback) -->
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome/all.min.css') }}">
     
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Playfair+Display:wght@600;700;800&display=swap" rel="stylesheet">
@@ -23,15 +23,15 @@
 <body>
     @yield('content')
 
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- jQuery (local) -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}" defer></script>
     
-    <!-- Bootstrap 5 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap 5 JS (local) -->
+    <script src="{{ asset('vendor/bootstrap/bootstrap.bundle.min.js') }}" defer></script>
     
-    <!-- GSAP -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+    <!-- GSAP (local) -->
+    <script src="{{ asset('vendor/gsap/gsap.min.js') }}" defer></script>
+    <script src="{{ asset('vendor/gsap/ScrollTrigger.min.js') }}" defer></script>
 
     <!-- Custom JS -->
     <script src="{{ asset('js/main.js') }}"></script>
